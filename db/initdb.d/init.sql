ca12d8b947e33d6cdf94352fcdd2ba7764c03fe4
yuichiyasui <yassan.b.a.w@gmail.com>
2024-05-07 00:00:41 +0900

    Add sql

---
+++ db/initdb.d/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS golang_todo_api;
+USE golang_todo_api;
+
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    description TEXT,
+    status ENUM('TODO', 'IN_PROGRESS', 'DONE')
+);
