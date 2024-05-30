27b077f6c4eaaa5714e763fbcd40506ba07f75ae
yuichiyasui <yassan.b.a.w@gmail.com>
2024-05-31 02:40:49 +0900

    Fix task list styles

--- app/src/routes/index.lazy.tsx
+++ app/src/routes/index.lazy.tsx
@@ -2,6 +2,16 @@ import { createLazyFileRoute, Link } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { components } from "@/api/__generated__/schema";
 import { client } from "@/api/api";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/utils";
 
 const Page = () => {
   const [tasks, setTasks] = useState<components["schemas"]["Task"][]>([]);
@@ -17,26 +27,40 @@ const Page = () => {
   }, []);
 
   return (
-    <main className="py-4 px-6">
-      <h1 className="text-xl font-bold mb-4">Task App</h1>
-      <ul className="mb-4">
-        {tasks.map((t) => (
-          <li key={t.id}>
-            <p>{t.id}</p>
-            <p>{t.title}</p>
-            <p>{t.description}</p>
-            <p>{t.status}</p>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <Link
-          to="/new"
-          className="bg-blue-500 px-4 py-1 rounded text-white font-bold"
-        >
+    <main className={cn("py-4", "px-6")}>
+      <h1 className={cn("text-lg", "font-bold", "mb-3")}>タスク一覧</h1>
+      <div className={cn("border", "rounded")}>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className={cn("w-20")}>ID</TableHead>
+              <TableHead>タスク名</TableHead>
+              <TableHead>状態</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {tasks.map((t) => (
+              <TableRow key={t.id}>
+                <TableCell>{t.id}</TableCell>
+                <TableCell>
+                  <p>{t.title}</p>
+                  {t.description && (
+                    <p className={cn("text-xs", "mt-1", "text-slate-400")}>
+                      {t.description}
+                    </p>
+                  )}
+                </TableCell>
+                <TableCell>{t.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <Button asChild>
+        <Link to="/new" className="mt-4">
           タスクを追加
         </Link>
-      </div>
+      </Button>
     </main>
   );
 };
