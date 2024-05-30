78cf020b257b3e3f0eb04b1ac87f52392f9f8472
yuichiyasui <yassan.b.a.w@gmail.com>
2024-05-31 00:23:50 +0900

    Setup shadcn/ui

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-import "./global.css";
+import "@/styles/global.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
